abcd5e492419f71616a8f8a53cbe483673b54af2
Braytiner <braytiner@usersystem.com.br>
2025-03-18 14:31:57 -0300

    Adiciona estrutura inicial da aplicação MAUI
    Este commit inclui a criação da solução e dos arquivos essenciais para a aplicação "GeradorDeNumerosAleatorios". Foram adicionados arquivos como `App.xaml`, `MainPage.xaml`, e suas respectivas classes de código por trás, definindo a interface e a lógica da aplicação. Também foram configurados os arquivos de manifesto para Android e iOS, como `AndroidManifest.xml` e `Info.plist`, essenciais para a execução em diferentes plataformas.
    
    Além disso, novos recursos visuais foram incluídos, como ícones e fontes, e foram criados arquivos para definir cores e estilos, garantindo uma aparência consistente e atraente na interface do usuário. Essas mudanças estabelecem a base para uma aplicação multiplataforma utilizando .NET MAUI.

---
+++ GeradorDeNumerosAleatorios/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GeradorDeNumerosAleatorios
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent(); //Leia o XAML e carregue os componentes
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GeradorDeNumerosAleatorios/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GeradorDeNumerosAleatorios
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent(); 
+        }
+    }
+}
